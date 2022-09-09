32052e9f87494d4b927bc21e7969d9566c28de76
Antoni Danov <antoni.danov@gmail.com>
2022-09-09 21:25:50 +0200

    Create RetrieveController only for retrieve shorted urls from  DB

--- UrlShortener/Services/IShortServices.cs
+++ UrlShortener/Services/IShortServices.cs
@@ -5,6 +5,7 @@ namespace UrlShortener.Services
     public interface IShortServices
     {
         public UrlData GetNewUrl (string data);
+        public string GetOriginalUrl(string data);
         public void CreateUrlRecord(UrlData data);
 
         public bool isCreated(string originalUrl);
