ad9be04d757f80292f21e63519981c98cd98ffc7
Antoni Danov <antoni.danov@gmail.com>
2022-09-14 15:00:41 +0200

    Adding first steps to Firebase authentiction with email and password

--- UrlShortener/ClientApp/src/app/app.module.ts
+++ UrlShortener/ClientApp/src/app/app.module.ts
@@ -13,6 +13,14 @@ import { ClipboardModule } from 'ngx-clipboard';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
+//firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,14 +30,18 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
     ShortUrlComponent,
     SignInComponent,
     SignUpComponent,
-    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ClipboardModule
+    ClipboardModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UrlShortener/ClientApp/src/environments/environment.ts
+++ UrlShortener/ClientApp/src/environments/environment.ts
@@ -1,6 +1,14 @@
 export const environment = {
   production: false,
   localhost: 'https://localhost:44373/api/data',
-  urlAddress: 'https://localhost:44373/'
+  urlAddress: 'https://localhost:44373/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBjfBreCdorHdFJ1eSafidxWN0Uut9PQ-Y",
+    authDomain: "urlshortener-bafd0.firebaseapp.com",
+    projectId: "urlshortener-bafd0",
+    storageBucket: "urlshortener-bafd0.appspot.com",
+    messagingSenderId: "543969418433",
+    appId: "1:543969418433:web:8375c03befbedbb90fc840"
+  }
 };
 
