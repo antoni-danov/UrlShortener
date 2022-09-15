36c78ffeccc6c7ecae7681b6fedc9f2d940516e4
Antoni Danov <antoni.danov@gmail.com>
2022-09-15 11:14:21 +0200

    Implement SignIn and SignOut method, Create CookieFactory

--- UrlShortener/ClientApp/src/app/components/navbar/navbar.component.ts
+++ UrlShortener/ClientApp/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServicesService } from '../../services/auth/auth-services.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean = true;
+
+  constructor(
+    private services: AuthServicesService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  Logout() {
+    this.isLoggedIn = false;
+    return this.services.SignOut();
   }
 
 }
--- UrlShortener/ClientApp/src/app/services/auth/auth-services.service.ts
+++ UrlShortener/ClientApp/src/app/services/auth/auth-services.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase/compat/app';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServicesService {
+  currentDate!: Date;
+  uid!: string;
+  jwt!: string;
+  email!: string;
 
   constructor(
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private cookieService: CookieService,
+    private router: Router
   ) { }
 
-  SignUpWithEmailAndPassword(email: string, password: string) {
+  async SignInWithEmailAndPassword(email: string, password: string) {
+    await this.afAuth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.uid = result.user?.uid!;
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+    var currentJwt = await this.getIdToken();
+    this.jwt = currentJwt!;
+    this.email = email;
 
-    return this.afAuth.createUserWithEmailAndPassword(email, password)
+    this.cookiesFactory(this.jwt, this.uid, this.email);
+
+    this.router.navigateByUrl('/');
+
+  }
+  async SignUpWithEmailAndPassword(email: string, password: string) {
+
+    return await this.afAuth.createUserWithEmailAndPassword(email, password)
       .then((result) => {
         const user = result.user;
       })
@@ -20,5 +47,26 @@ export class AuthServicesService {
         const errorCode = error.code;
         const errorMessage = error.message;
       });
+  }
+  async SignOut() {
+    this.cookieService.deleteAll();
+
+    return await this.afAuth.signOut()
+      .then(() => {
+        this.router.navigate(['/']);
+      });
+  }
+
+  cookiesFactory(jwt: string, uid: string, email: string) {
+
+    this.currentDate = new Date();
+    this.currentDate.setHours(this.currentDate.getHours() + 12);
+
+    this.cookieService.set('JWT', jwt, { expires: this.currentDate, secure: true });
+    this.cookieService.set('uid', uid, { expires: this.currentDate, secure: true });
+    this.cookieService.set('email', email, { expires: this.currentDate, secure: true });
+  }
+  async getIdToken() {
+    return await firebase.default.auth().currentUser?.getIdToken();
   }
 }
