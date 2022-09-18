4d1a186274fb593bbf59ad3e22ed4af0dbcf49b0
Antoni Danov <antoni.danov@gmail.com>
2022-09-18 10:45:23 +0200

    Create detailComponent and editComponent Style a user profile page

---
+++ UrlShortener/ClientApp/src/app/components/url-details/url-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-url-details',
+  templateUrl: './url-details.component.html',
+  styleUrls: ['./url-details.component.css']
+})
+export class UrlDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
